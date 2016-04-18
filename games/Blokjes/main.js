a96d8b7e74f0bdb0008c32e5cca999ef03eb6936
Guido Soetens <guido@activecues.com>
2016-04-18 18:12:17 +0200

    added physics

--- games/Blokjes/main.js
+++ games/Blokjes/main.js
@@ -1,16 +1,30 @@
 ///<reference path="../../phaser/phaser.d.ts"/>
 var SimpleGame = (function () {
     function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(640, 480, Phaser.AUTO, 'content', {
+            create: this.create, preload: this.preload, render: this.render
+        });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', '../../assets/sprites/phaser2.png');
+        this.game.load.image("decepticon", "../../assets/sprites/arrow.png");
+    };
+    SimpleGame.prototype.render = function () {
+        // This renders debug information about physics bodies
+        //this.game.debug.body(this.player);
     };
     SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-        logo.scale.setTo(0.2, 0.2);
-        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+        this.player = this.game.add.sprite(this.game.width / 2, 0, "decepticon");
+        // Start the ARCADE Physics system
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        // Enable physics on the player sprite
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        // Set the sprite to collide with the worlds edge
+        this.player.body.collideWorldBounds = true;
+        // And set bounce in the Y axis ( called restitution in most physics system ) to 1, 
+        // which will make it bounce equal to 100 %
+        this.player.body.bounce.y = 0.5;
+        // Set the physics engines overall gravity.  98 == 98 pixels per second in this demo
+        this.game.physics.arcade.gravity.y = 98;
     };
     return SimpleGame;
 }());
