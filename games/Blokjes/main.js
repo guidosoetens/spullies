55ad565f9928a69f6ef56eff5948a4303f3eb80f
Guido Soetens <guido@activecues.com>
2016-04-28 16:43:36 +0200

    yoloswaggeries

--- games/Blokjes/main.js
+++ games/Blokjes/main.js
@@ -2,18 +2,18 @@
 var SimpleGame = (function () {
     function SimpleGame() {
         this.game = new Phaser.Game(640, 480, Phaser.AUTO, 'content', {
-            create: this.create, preload: this.preload, render: this.render
+            create: this.create, preload: this.preload, render: this.render, onmousedown: this.onmousedown
         });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image("decepticon", "../../assets/sprites/aqua_ball.png");
+        this.game.load.image("balletje", "../../assets/sprites/aqua_ball.png");
     };
     SimpleGame.prototype.render = function () {
         // This renders debug information about physics bodies
         //this.game.debug.body(this.player);
     };
     SimpleGame.prototype.create = function () {
-        this.player = this.game.add.sprite(this.game.width / 2, 0, "decepticon");
+        this.player = this.game.add.sprite(this.game.width / 2, 0, "balletje");
         // Start the ARCADE Physics system
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         // Enable physics on the player sprite
@@ -25,6 +25,18 @@ var SimpleGame = (function () {
         this.player.body.bounce.y = 0.5;
         // Set the physics engines overall gravity.  98 == 98 pixels per second in this demo
         this.game.physics.arcade.gravity.y = 98;
+        this.graphics = this.game.add.graphics(100, 100);
+        this.graphics.clear();
+        this.graphics.beginFill(0xFF33FF);
+        this.graphics.drawCircle(200, 200, 100);
+        this.graphics.endFill();
+    };
+    SimpleGame.prototype.onmousedown = function (ev) {
+        alert("mouse down...");
+        this.graphics.clear();
+        this.graphics.beginFill(0xFF3300);
+        this.graphics.drawCircle(200, 200, 100);
+        this.graphics.endFill();
     };
     return SimpleGame;
 }());
--- games/Blokjes/main.ts
+++ games/Blokjes/main.ts
@@ -3,14 +3,15 @@
 class SimpleGame {
     game: Phaser.Game;
     player: Phaser.Sprite;
+    graphics: Phaser.Graphics;
 
     constructor() {
         this.game = new Phaser.Game(640, 480, Phaser.AUTO, 'content', {
-            create: this.create, preload:this.preload, render: this.render
+            create: this.create, preload:this.preload, render: this.render, onmousedown: this.onmousedown
         });
     }
     preload() {
-        this.game.load.image("decepticon", "../../assets/sprites/aqua_ball.png");
+        this.game.load.image("balletje", "../../assets/sprites/aqua_ball.png");
     }
     render() {
         // This renders debug information about physics bodies
@@ -18,7 +19,7 @@ class SimpleGame {
     }
     create() {
         
-        this.player = this.game.add.sprite(this.game.width / 2, 0, "decepticon");
+        this.player = this.game.add.sprite(this.game.width / 2, 0, "balletje");
 
         // Start the ARCADE Physics system
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -34,6 +35,30 @@ class SimpleGame {
 
         // Set the physics engines overall gravity.  98 == 98 pixels per second in this demo
         this.game.physics.arcade.gravity.y = 98;
+        
+        
+        this.graphics = this.game.add.graphics(100, 100);
+        
+        this.graphics.clear();
+        this.graphics.beginFill(0xFF33FF);
+        this.graphics.drawCircle(200,200,100);
+        this.graphics.endFill();
+    }
+    
+    onmousedownz() {
+        alert("mouse down...");
+        this.graphics.clear();
+        this.graphics.beginFill(0xFF3300);
+        this.graphics.drawCircle(200,200,100);
+        this.graphics.endFill();
+    }
+    
+    onmousedown(ev:MouseEvent) {
+        alert("mouse down...");
+        this.graphics.clear();
+        this.graphics.beginFill(0xFF3300);
+        this.graphics.drawCircle(200,200,100);
+        this.graphics.endFill();
     }
 }
 
