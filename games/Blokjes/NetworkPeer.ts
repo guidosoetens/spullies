b7b20e119f3bfff47bff58f7097eb49fdcbaf887
Guido Soetens <guido@activecues.com>
2016-05-09 17:49:39 +0200

    added socket.io

---
+++ games/Blokjes/NetworkPeer.ts
@@ -0,0 +1,11 @@
+///<reference path="../../libs/socket.io.d.ts"/>
+///<reference path="../../libs/socket.io-client.d.ts"/>
+
+module BlokjesGame {
+    
+    
+    export class NetworkPeer {
+        
+        
+    }
+}
