ba8313f1c79cd9532d8178e57306563d8669c592
Guido Soetens <guido@activecues.com>
2016-05-19 18:05:44 +0200

    edits

--- games/Uitprobeersel/main.ts
+++ games/Uitprobeersel/main.ts
@@ -4,22 +4,27 @@ module BlokjesGame
 {
     const WIDTH:number = 800;
     const HEIGHT:number = 600;
-    const SCALE:number = 0.5;
+    const SCALE:number = 0.25;
     const MOTION_WIDTH:number = WIDTH * SCALE;
     const MOTION_HEIGHT:number = HEIGHT * SCALE;
-    const MOTION_RADIUS:number = 30;
+    const MOTION_RADIUS:number = 20;
     
     export class SimpleGame {
         
         game: Phaser.Game;
         shader: Phaser.Filter;
-        bitmapData:Phaser.BitmapData;
         inputLoc:Phaser.Point;
-        renderTexture:Phaser.RenderTexture;
-        image:Phaser.Image;
+        
+        motionImageBmpData:Phaser.BitmapData;
+        motionImage:Phaser.Image;
+         
+        velocityMapTarget:Phaser.RenderTexture;
+        velocityMapTargetContainer:Phaser.Image;
+        textureTargetBitmap:Phaser.RenderTexture;
+        textureTargetBitmapContainer:Phaser.Image;
         
         constructor() {
-            this.game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, render: this.render });
+            this.game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, preRender: this.preRender, render: this.render });
         }
         
         preload() {
@@ -27,36 +32,50 @@ module BlokjesGame
         }
         
         create() {
+            
             this.shader = new Phaser.Filter(this.game, null, this.game.cache.getShader('velocityShader'));
-            
             this.inputLoc = new Phaser.Point(0, 0);
-            
-            //Create Stamp:
-            this.bitmapData = this.game.make.bitmapData(MOTION_WIDTH, MOTION_HEIGHT);
-            this.image = this.game.add.image(0, 0, this.bitmapData);
-            this.image.scale.x = 1.0 / SCALE;
-            this.image.scale.y = 1.0 / SCALE;
-            this.image.filters = [ this.shader ];
-            
-            this.renderTexture = this.game.add.renderTexture(MOTION_WIDTH, MOTION_HEIGHT);
            
-            //this.game.add.image(0,0,this.renderTexture);
+            this.velocityMapTarget = this.game.make.renderTexture(MOTION_WIDTH, MOTION_HEIGHT);
+            this.velocityMapTargetContainer = this.game.add.image(0,0,this.velocityMapTarget);
+            this.textureTargetBitmap = this.game.make.renderTexture(MOTION_WIDTH, MOTION_HEIGHT);
+            this.textureTargetBitmap.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+            this.textureTargetBitmapContainer = this.game.add.image(MOTION_WIDTH,0,this.textureTargetBitmap);
+           
+            //motion image:
+            this.motionImageBmpData = this.game.make.bitmapData(MOTION_WIDTH, MOTION_HEIGHT);
+            this.motionImage = this.game.add.image(0, 0, this.motionImageBmpData);
+            this.motionImage.scale.x = 1.0 / SCALE;
+            this.motionImage.scale.y = 1.0 / SCALE;
+            this.motionImage.filters = [ this.shader ];
+            this.motionImage.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+            
+            this.shader.uniforms.uPreviousFrame = { type: 'sampler2D', value: this.textureTargetBitmapContainer.texture, textureData: { repeat: false } };
+            this.shader.uniforms.uResolution = { type: '2f', value: { x: MOTION_WIDTH, y: MOTION_HEIGHT } };
         }
         
         update() {
             
+            var mat:Phaser.Matrix = new Phaser.Matrix().identity().scale(1.0, -1.0).translate(0, MOTION_HEIGHT);
+            this.velocityMapTarget.render(this.motionImage, mat);
+            mat = new Phaser.Matrix().identity().scale(1.0, 1.0);
+            this.textureTargetBitmap.render(this.velocityMapTargetContainer, mat);
+            
             //update location buffer:
             var prevLoc = this.inputLoc;
             var currLoc = new Phaser.Point(this.inputLoc.x, this.inputLoc.y).multiply(.9, .9).add(.1 * SCALE * this.game.input.position.x, .1 * SCALE * this.game.input.position.y);
+            //clamp location:
+            currLoc.x = Math.round(currLoc.x);
+            currLoc.y = Math.round(currLoc.y);
             this.inputLoc = currLoc;
             
-            this.bitmapData.clear();
-            this.bitmapData.update();
+            this.motionImageBmpData.fill(0,0,0);
+            this.motionImageBmpData.update();
             
-            var minX:number = Math.round(Math.min(prevLoc.x - MOTION_RADIUS, currLoc.x - MOTION_RADIUS));
-            var maxX:number = Math.round(Math.max(prevLoc.x + MOTION_RADIUS, currLoc.x + MOTION_RADIUS));
-            var minY:number = Math.round(Math.min(prevLoc.y - MOTION_RADIUS, currLoc.y - MOTION_RADIUS));
-            var maxY:number = Math.round(Math.max(prevLoc.y + MOTION_RADIUS, currLoc.y + MOTION_RADIUS));
+            var minX:number = Math.round(Math.min(prevLoc.x - MOTION_RADIUS, currLoc.x - MOTION_RADIUS)) - 1;
+            var maxX:number = Math.round(Math.max(prevLoc.x + MOTION_RADIUS, currLoc.x + MOTION_RADIUS)) + 1;
+            var minY:number = Math.round(Math.min(prevLoc.y - MOTION_RADIUS, currLoc.y - MOTION_RADIUS)) - 1;
+            var maxY:number = Math.round(Math.max(prevLoc.y + MOTION_RADIUS, currLoc.y + MOTION_RADIUS)) + 1;
           
             var radSqrd = MOTION_RADIUS * MOTION_RADIUS;
             
@@ -66,17 +85,20 @@ module BlokjesGame
                     var closeTo1 = new Phaser.Point(prevLoc.x - x, prevLoc.y - y).getMagnitudeSq() <= radSqrd;
                     var closeTo2 = new Phaser.Point(currLoc.x - x, currLoc.y - y).getMagnitudeSq() <= radSqrd;
                     if(closeTo1 && !closeTo2 || !closeTo1 && closeTo2)
-                        this.bitmapData.setPixel(x, y, 255, 0, 0, false);
+                        this.motionImageBmpData.setPixel(x, y, 255, 255, 255, false);
                 }
             }
-            this.bitmapData.setPixel(0,0,0,0,0);
+            this.motionImageBmpData.setPixel(0,0,0,0,0);
             
-            //this.renderTexture.clear();
-            this.renderTexture.render(this.image);
+            this.shader.update();
+        }
+        
+        preRender() {
+            
         }
         
         render() {
-            //this.game.debug.text("input loc: " + this.inputLoc, 0, 50);
+            
         }
     }
 }
--- games/Uitprobeersel/velocityShader.frag
+++ games/Uitprobeersel/velocityShader.frag
@@ -1,18 +1,76 @@
 precision mediump float;
 
-/*
 //phaser variables:
 uniform float time;
 uniform vec2 resolution;
 uniform vec2 mouse;
-*/
 
 //phaser base texture:
 varying vec2 vTextureCoord;
-uniform sampler2D uSampler;
+uniform sampler2D uSampler; //the motion image is stored in uSampler...
+
+uniform vec2 uResolution;
+uniform sampler2D uPreviousFrame;
+
+#define KERNEL_FROM -3
+#define KERNEL_TO 4
+
+void renderCircle(vec2 uv){
+    vec2 to = 2.0 * (uv - vec2(.5));
+    float len = length(to);
+    if(len < 1.0 && len > 0.8) {
+        gl_FragColor = vec4(1.0, .5 * to.x + 0.5, 0.5 * to.y + 0.5, 1);
+    }
+}
 
 void main( void )
 {
-    vec4 texColor = texture2D(uSampler, vTextureCoord);
-    gl_FragColor = vec4(texColor.x, vTextureCoord.x, vTextureCoord.y, 1.0);
+    float motionPt = texture2D(uSampler, vTextureCoord).r;
+    vec3 prevValue = texture2D(uPreviousFrame, vTextureCoord).xyz;
+    
+    //float val = max(motionPt, prevValue.x *.95);
+    
+    float val = texture2D(uSampler, vTextureCoord + vec2(1.0 / uResolution.x, 0)).r;
+    float dx = texture2D(uSampler, vTextureCoord + vec2(1.0 / uResolution.x, 0)).r;
+    float dy = texture2D(uSampler, vTextureCoord + vec2(0, 1.0 / uResolution.x)).r;
+    
+    
+    
+    
+    gl_FragColor = vec4(val, 0, 0, 1);
+    
+    float circleHeight = .15 * uResolution.x / uResolution.y;
+    if(vTextureCoord.x > .05 && vTextureCoord.x < .2 && vTextureCoord.y < .95 && vTextureCoord.y > .95 - circleHeight)
+        renderCircle((vTextureCoord - vec2(.05, .95 - circleHeight)) * vec2(1.0 / 0.15, 1.0 / circleHeight));
+    
+    
+    /*
+    float prev = texture2D(uPreviousFrame, vTextureCoord).r;
+    
+    //apply kernel:
+    vec2 sumTo = 
+    for(int i=KERNEL_FROM; i<KERNEL_TO; ++i) {
+        for(int j=KERNEL_FROM; j<KERNEL_TO; ++j) {
+            
+            vec2 uv = vTextureCoord + vec2(j / resolution.x, i / resolution.y);
+            vec3 clr = texture2D(uPreviousFrame, uv).xyz;
+            
+            
+            
+            
+            
+            vec2 n = vec2(2.0 * clr.x - 1.0, 2.0 * clr.y - 1.0);
+            sumNormal += n;
+        }
+    }
+    
+    
+    
+    float curr = texture2D(uSampler, vTextureCoord).r;
+    float v = max(curr, prev);
+    
+    vec2 normal = vec2(.5, .5);
+    
+    gl_FragColor = vec4(.5 + .5 * normal.x, .5 + .5 * normal.y, curr, 1);
+    */
 }
